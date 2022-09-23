01602c7f6e94a817093d10b2ba1f58dd8f8ee21b
Claudiu Neamtu <claudiu.neamtu.31@gmail.com>
2022-09-23 18:03:18 +0300

    update github action - v4

--- BackEnd/router.ts
+++ BackEnd/router.ts
@@ -1,10 +1,9 @@
 import { Router } from "https://deno.land/x/oak@v11.1.0/router.ts";
 import { authRouter } from "./api/auth/router.ts";
 import { usersRouter } from "./api/users/router.ts";
-import { createAccessToken } from "./auth/index.ts";
 
 export const apiRouter = new Router();
 
-apiRouter.get("/", async (context) => { context.response.body = { status: "active", version: "1.0.0", a: await createAccessToken({ _id: "123" }) } })
+apiRouter.get("/", (context) => { context.response.body = { status: "active", version: "1.0.0"} })
 apiRouter.use("/auth", authRouter.routes(), authRouter.allowedMethods());
 apiRouter.use("/users", usersRouter.routes(), usersRouter.allowedMethods());
