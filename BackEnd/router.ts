f68e1b1760717bbd56c54ab5a812f1cf6911fccd
Claudiu Neamtu <claudiu.neamtu.31@gmail.com>
2022-09-23 18:43:46 +0300

    update github action - v16

--- BackEnd/router.ts
+++ BackEnd/router.ts
@@ -4,6 +4,6 @@ import { usersRouter } from "./api/users/router.ts";
 
 export const apiRouter = new Router();
 
-apiRouter.get("/", (context) => { context.response.body = { status: "active", version: "1.0.0"} })
-apiRouter.use("/auth", authRouter.routes(), authRouter.allowedMethods());
-apiRouter.use("/users", usersRouter.routes(), usersRouter.allowedMethods());
+apiRouter.get("api/", (context) => { context.response.body = { status: "active", version: "1.0.0"} })
+apiRouter.use("api/auth", authRouter.routes(), authRouter.allowedMethods());
+apiRouter.use("api/users", usersRouter.routes(), usersRouter.allowedMethods());
