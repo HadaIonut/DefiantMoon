10ad5a4a891babbe55754bb372a1bd9fea45ba9f
ionut <hadaionut@gmail.com>
2022-09-30 20:24:25 +0300

    added input field and placeholder toolbar

--- FrontEnd/src/components/windowBodies/ChatWindowBody.vue
+++ FrontEnd/src/components/windowBodies/ChatWindowBody.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import {QuillEditor} from '@vueup/vue-quill'
 import '@vueup/vue-quill/dist/vue-quill.bubble.css'
+
+const chatInputContent: string = ''
 </script>
 
 <template>
@@ -8,24 +10,38 @@ import '@vueup/vue-quill/dist/vue-quill.bubble.css'
         <div class="chat-content">
 
         </div>
-        <div class="chat-input">
-            <QuillEditor theme="bubble"/>
+        <div class="chat-input-container">
+            <div class="chat-toolbar">
+                a
+            </div>
+            <div class="chat-input">
+                <QuillEditor theme="bubble" v-model="chatInputContent"/>
+            </div>
         </div>
     </div>
 </template>
 
 <style scoped lang="scss">
+$chat-input-height: 80px;
+$content-margin-bottom: 15px;
+$parent-padding: 5px;
+
 .chat {
     height: 100%;
 }
 .chat-content {
     border: 1px solid $background;
-    margin-bottom: 15px;
+    margin-bottom: $content-margin-bottom;
+    height: calc(100% - #{$chat-input-height} - #{$content-margin-bottom} - #{$parent-padding});
 }
-.chat-input {
-    width: 100%;
+.chat-input-container {
     border: 1px solid $background;
     background: $darker-secondary;
     border-radius: 10px;
+    height: $chat-input-height;
+}
+
+.ql-container {
+    height: calc(100% - 36px);
 }
 </style>
