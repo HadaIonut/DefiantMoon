b77de85b192565f2b32b3e15f30dbcea4b8dd1a6
ionut <hadaionut@gmail.com>
2022-10-08 09:57:03 +0300

    moved toolbar to separate component

--- FrontEnd/src/components/windowBodies/ChatWindowBody.vue
+++ FrontEnd/src/components/windowBodies/ChatWindowBody.vue
@@ -1,17 +1,12 @@
 <script setup lang="ts">
 import {Quill, QuillEditor} from '@vueup/vue-quill'
 import '@vueup/vue-quill/dist/vue-quill.bubble.css'
-import DiceD4Icon from '../customIcons/DiceD4Icon.vue'
-import DiceD6Icon from '../customIcons/DiceD6Icon.vue'
-import DiceD8Icon from '../customIcons/DiceD8Icon.vue'
-import DiceD10Icon from '../customIcons/DiceD10Icon.vue'
-import DiceD12Icon from '../customIcons/DiceD12Icon.vue'
-import DiceD20Icon from '../customIcons/DiceD20Icon.vue'
 import {onMounted, ref, Ref} from 'vue'
 import {Window} from 'types/windows'
 import {getRandomString} from '../../utils/utils'
 import {WEBSOCKET_EMITABLE_EVENTS, WEBSOCKET_RECEIVABLE_EVENTS} from '../../websocket/events'
 import {websocket} from '../../websocket/websocket'
+import ChatToolbar from '../chatComponents/ChatToolbar.vue'
 
 type ChatMessage = {
     from: string;
@@ -25,33 +20,6 @@ const props = defineProps<{ windowData: Window }>()
 const chatEditor: Ref<Quill> = ref('')
 const chatMessages: Ref<ChatMessage[]> = ref([])
 const uploadedImages: Ref<string[]> = ref([])
-
-const diceButton = (diceType: string) => {
-    const currentContent = chatEditor.value.getHTML()
-
-    const hasOtherText: boolean = !!currentContent.match(/<p>([/ \w\n+]+)<\/p>/)?.[1]
-    const hasOnlyBR: boolean = currentContent === '<p><br></p>'
-    const append: boolean = hasOtherText && !hasOnlyBR
-    const hasRollFormula: boolean = currentContent.includes('/r')
-    const currentDiceRegex = new RegExp('([0-9]+' + diceType + ')')
-    const containsThisDiceType: boolean = !!currentContent.match(currentDiceRegex)?.[1]
-
-    if (append) {
-        let newChatValue: string = ''
-
-        if (hasRollFormula && containsThisDiceType) {
-            const diceRegex = new RegExp(`([0-9]+)${diceType}`)
-
-            newChatValue = currentContent.replace(diceRegex, (_: string, diceCount: string) => (Number(diceCount) + 1) + diceType)
-        } else if (hasRollFormula) {
-            const rollFormula = new RegExp('/r((?: ?\\dd\\d+ ?\\+?)+)')
-
-            newChatValue = currentContent.replace(rollFormula, (_: string, formula: string) => `/r${formula} + 1${diceType}`)
-        } else newChatValue = `${chatEditor.value.getHTML()}<p>/r 1${diceType}</p>`
-
-        chatEditor.value.setHTML(newChatValue)
-    } else chatEditor.value.setHTML(`<p>/r 1${diceType}</p>`)
-}
 
 const isImageUrl = (text: string): boolean => {
     return new RegExp('(http(s?):)([/|.|\\w|\\s|-])*\\.(?:jpg|gif|png)').test(text)
@@ -122,7 +90,6 @@ const sendMessage = async () => {
     const images: File[] = await getImages()
 
     const imagesBlobs = await Promise.all(images.map(blobToBase64))
-    console.log(JSON.stringify(imagesBlobs))
 
     websocket.sendMessage(WEBSOCKET_EMITABLE_EVENTS.CHAT_MESSAGE, {text: currentContent, images: imagesBlobs})
     chatEditor.value.setText('')
@@ -179,9 +146,11 @@ const getFormattedTextMessage = (chatMessage: ChatMessage) => {
     <div :class="`chat ${props.windowData.isMinimized ? 'minimized' : ''}`">
         <div class="chat-content">
             <perfect-scrollbar>
-                <div v-for="message in chatMessages" class="message-row">
-                    {{ getFormattedTextMessage(message) }}
-                    <img class="chat-image" v-for="image in message.images" :src="image">
+                <div v-for="message in chatMessages" class="message-row" :key="message.timestamp">
+                    <span :key="message.timestamp">
+                        {{ getFormattedTextMessage(message) }}
+                        <img class="chat-image" v-for="image in message.images" :src="image" alt="">
+                    </span>
                 </div>
             </perfect-scrollbar>
         </div>
@@ -192,30 +161,7 @@ const getFormattedTextMessage = (chatMessage: ChatMessage) => {
                          class="uploaded-image">
                 </perfect-scrollbar>
             </div>
-            <div class="chat-toolbar">
-                <div class="dice d4 toolbar-action" @click="() => diceButton('d4')">
-                    <DiceD4Icon size="28px"/>
-                </div>
-                <div class="dice d6 toolbar-action" @click="() => diceButton('d6')">
-                    <DiceD6Icon size="28px"/>
-                </div>
-                <div class="dice d8 toolbar-action" @click="() => diceButton('d8')">
-                    <DiceD8Icon size="28px"/>
-                </div>
-                <div class="dice d10 toolbar-action" @click="() => diceButton('d10')">
-                    <DiceD10Icon size="28px"/>
-                </div>
-                <div class="dice d12 toolbar-action" @click="() => diceButton('d12')">
-                    <DiceD12Icon size="28px"/>
-                </div>
-                <div class="dice d20 toolbar-action" @click="() => diceButton('d20')">
-                    <DiceD20Icon size="28px"/>
-                </div>
-                <div class="send-message toolbar-action">
-                    Send
-                    <font-awesome-icon icon="fa-solid fa-paper-plane"/>
-                </div>
-            </div>
+            <ChatToolbar :chatEditor="chatEditor"/>
             <div class="chat-input">
                 <QuillEditor theme="bubble" ref="chatEditor"/>
             </div>
@@ -257,15 +203,6 @@ $parent-padding: 5px;
     width: 100%;
 }
 
-.chat-toolbar {
-    display: flex;
-    height: $chat-toolbar-height;
-    align-items: center;
-    padding-left: 10px;
-    padding-right: 10px;
-    border-bottom: 1px dashed $background;
-}
-
 .chat-input-container {
     border: 1px solid $background;
     background: $darker-secondary;
@@ -276,27 +213,6 @@ $parent-padding: 5px;
 
 .chat-input {
     height: calc(100% - #{$chat-toolbar-height});
-}
-
-.toolbar-action {
-    cursor: pointer;
-    background: $darker-secondary;
-    transition: background-color 0.2s ease-in-out;
-    padding-left: 2px;
-    padding-right: 2px;
-
-    &:hover {
-        background: $darker2-secondary;
-    }
-}
-
-.send-message {
-    margin-left: auto;
-    user-select: none;
-    height: 100%;
-    display: flex;
-    align-items: center;
-    grid-gap: 5px;
 }
 
 .dice {
