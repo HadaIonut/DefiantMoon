a36edf6636d3bc5efe831344596e0d596dbf980a
ionut <hadaionut@gmail.com>
2022-11-07 11:02:48 +0200

    parsed items.json

--- BackEnd/dndDataFormatter/index.ts
+++ BackEnd/dndDataFormatter/index.ts
@@ -1,63 +1,91 @@
-import { skillToAbilityMap, getEmptySkillsObject, damageTypeConversionMap} from "./constants/constants.ts"
-import { createItemAction, extractDamage, extractItemRange, parseAbilityModifiers, parseAttunementRequirements, parseItemArmor, parseItemBonuses, parseItemDescription, parseItemProperties } from "./itemParser.ts"
-import { parseDescription, parseMonsterTrait } from "./traitParser.ts"
-import { Item} from "./types/Items.d.ts"
-import { Monster, MonsterAC, MonsterHP, MonsterResistance, MonsterSave, MonsterSkill, MonsterSpeed, MonsterSpeedValue } from "./types/Monster.d.ts"
-import { OrigianlItem} from "./types/OriginalItems.d.ts"
-import { OriginalAC, OriginalImmunity, OriginalMonster, OriginalResistance, OriginalSave, OriginalSkill, OriginalSpeed, OriginalSpeedWithCondition } from "./types/OriginalMonster.d.ts"
-import { generateRandomString } from "./utils.ts"
+import { skillToAbilityMap, getEmptySkillsObject, damageTypeConversionMap } from "./constants/constants.ts";
+import {
+    createItemAction,
+    extractDamage,
+    extractItemRange,
+    parseAbilityModifiers,
+    parseAttunementRequirements,
+    parseItemArmor,
+    parseItemBonuses,
+    parseItemDescription,
+    parseItemProperties,
+} from "./itemParser.ts";
+import { parseDescription, parseMonsterTrait } from "./traitParser.ts";
+import { Item } from "./types/Items.d.ts";
+import {
+    Monster,
+    MonsterAC,
+    MonsterHP,
+    MonsterResistance,
+    MonsterSave,
+    MonsterSkill,
+    MonsterSpeed,
+    MonsterSpeedValue,
+} from "./types/Monster.d.ts";
+import { OrigianlItem } from "./types/OriginalItems.d.ts";
+import {
+    OriginalAC,
+    OriginalImmunity,
+    OriginalMonster,
+    OriginalResistance,
+    OriginalSave,
+    OriginalSkill,
+    OriginalSpeed,
+    OriginalSpeedWithCondition,
+} from "./types/OriginalMonster.d.ts";
+import { generateRandomString } from "./utils.ts";
 
-const bestiaryDir = Deno.readDir('./data/bestiary')
-const itemsDir = Deno.readDir("./data/items")
+const bestiaryDir = Deno.readDir("./data/bestiary");
+const itemsDir = Deno.readDir("./data/items");
 
 const parseMonsterAC = (originalAc: (OriginalAC | number)[]): MonsterAC[] => {
-    const out: MonsterAC[] = []
-    
+    const out: MonsterAC[] = [];
+
     originalAc?.forEach?.((ac: OriginalAC | number) => {
-        if (typeof ac === 'number') {
+        if (typeof ac === "number") {
             out.push({
                 value: ac,
                 source: [],
-                condition: ''
-            })
+                condition: "",
+            });
         } else {
             out.push({
                 value: ac.ac,
                 source: ac.from ?? [],
-                condition: ac.condition ?? ''
-            })
+                condition: ac.condition ?? "",
+            });
         }
-    })
+    });
 
-    return out
-}
+    return out;
+};
 
-const parseMonsterHP = (originalHp: {average: number | string, formula: string}): MonsterHP => {
+const parseMonsterHP = (originalHp: { average: number | string; formula: string }): MonsterHP => {
     return {
         average: Number(originalHp?.average ?? 0),
-        formula: originalHp?.formula ?? ''
-    }
-}
+        formula: originalHp?.formula ?? "",
+    };
+};
 
 const parseMonsterSpeed = (originalSpeed?: OriginalSpeed): MonsterSpeed => {
-    const speedTypes = ['walk', 'fly', 'climb', 'swim', 'borrow']
+    const speedTypes = ["walk", "fly", "climb", "swim", "borrow"];
     const out: {
-        [key: string]: MonsterSpeedValue | boolean
-    } = {}
+        [key: string]: MonsterSpeedValue | boolean;
+    } = {};
 
     speedTypes.forEach((type) => {
-        const speedValue = originalSpeed?.[type as keyof OriginalSpeed]
+        const speedValue = originalSpeed?.[type as keyof OriginalSpeed];
 
         out[type] = {
             value: (speedValue as OriginalSpeedWithCondition)?.number ?? speedValue ?? 0,
-            condition: (speedValue as OriginalSpeedWithCondition)?.condition ?? ''
-        }
-    })
+            condition: (speedValue as OriginalSpeedWithCondition)?.condition ?? "",
+        };
+    });
 
-    out.canHover = originalSpeed?.canHover ?? false
+    out.canHover = originalSpeed?.canHover ?? false;
 
-    return out as MonsterSpeed
-}
+    return out as MonsterSpeed;
+};
 
 const parseMonsterSave = (originalSave: OriginalSave = {}): MonsterSave => {
     return {
@@ -67,76 +95,76 @@ const parseMonsterSave = (originalSave: OriginalSave = {}): MonsterSave => {
         int: !!originalSave?.int,
         wis: !!originalSave?.wis,
         cha: !!originalSave?.cha,
-    }
-}
+    };
+};
 
-const statToModifier = (stat: number):number => Math.floor((stat - 10) / 2)
+const statToModifier = (stat: number): number => Math.floor((stat - 10) / 2);
 
 const getProficiencyScore = (monster: OriginalMonster): number => {
-    const saves = monster?.save ?? {}
+    const saves = monster?.save ?? {};
 
-    const saveSkill = Object.keys(saves)[0] ?? 'str'
-    const saveSkillValue = monster?.save?.[saveSkill as keyof OriginalSave] ?? '+0'
-    const statValue: number = monster[saveSkill as keyof OriginalMonster] as number ?? 10
-    const statModifier = statToModifier(statValue)
+    const saveSkill = Object.keys(saves)[0] ?? "str";
+    const saveSkillValue = monster?.save?.[saveSkill as keyof OriginalSave] ?? "+0";
+    const statValue: number = (monster[saveSkill as keyof OriginalMonster] as number) ?? 10;
+    const statModifier = statToModifier(statValue);
 
-    return Math.abs(Number(saveSkillValue) - statModifier)
-}
+    return Math.abs(Number(saveSkillValue) - statModifier);
+};
 
 const parseMonsterSkills = (originalSkill: OriginalSkill | undefined, proficiency: number, monster: OriginalMonster): MonsterSkill => {
-    const out = getEmptySkillsObject()
+    const out = getEmptySkillsObject();
 
     Object.entries(originalSkill ?? {}).forEach(([skill, value]) => {
-        const skillAbility = skillToAbilityMap[skill as keyof typeof skillToAbilityMap]
-        const abilityValue = statToModifier(monster[skillAbility as keyof OriginalMonster] as number)
-        const profMultiplier = (Number(value) - abilityValue) / proficiency
+        const skillAbility = skillToAbilityMap[skill as keyof typeof skillToAbilityMap];
+        const abilityValue = statToModifier(monster[skillAbility as keyof OriginalMonster] as number);
+        const profMultiplier = (Number(value) - abilityValue) / proficiency;
 
-        if (profMultiplier == 0.5) out[skill as keyof MonsterSkill] = 1
-        else if (profMultiplier == 1) out[skill as keyof MonsterSkill] = 2
-        else if (profMultiplier == 2) out[skill as keyof MonsterSkill] = 3
-        else out[skill as keyof MonsterSkill] = 0
-    })
+        if (profMultiplier == 0.5) out[skill as keyof MonsterSkill] = 1;
+        else if (profMultiplier == 1) out[skill as keyof MonsterSkill] = 2;
+        else if (profMultiplier == 2) out[skill as keyof MonsterSkill] = 3;
+        else out[skill as keyof MonsterSkill] = 0;
+    });
 
-    return out
-}
+    return out;
+};
 
 const parseMonsterResistance = (originalResistance?: (string | OriginalResistance | OriginalImmunity)[]): MonsterResistance[] => {
-    const out: MonsterResistance[] = []
+    const out: MonsterResistance[] = [];
 
     originalResistance?.forEach?.((resistace: string | OriginalResistance | OriginalImmunity) => {
-        if (typeof resistace === 'string') {
+        if (typeof resistace === "string") {
             out.push({
                 value: [resistace],
-                condition: 'false'
-            })
+                condition: "false",
+            });
         } else {
             out.push({
                 value: (resistace as OriginalResistance)?.resist ?? (resistace as OriginalImmunity)?.immune,
-                condition: resistace?.note ?? ''
-            })
+                condition: resistace?.note ?? "",
+            });
         }
-    })
+    });
 
-    return out
-}
+    return out;
+};
 
 for await (const file of itemsDir) {
-    if (!file.isFile) continue
+    if (!file.isFile) continue;
 
-    const itemsList = JSON.parse(await Deno.readTextFile(`./data/items/${file.name}`))
+    const itemsList = JSON.parse(await Deno.readTextFile(`./data/items/${file.name}`));
 
-    const parsedItemList: Item[] = []
+    const parsedItemList: Item[] = [];
 
-    if (!itemsList.baseitem) continue
+    if (!itemsList.baseitem && !itemsList.item) continue;
 
-    itemsList.baseitem.forEach((item: OrigianlItem) => {
-        const parsedItem: Item  = {
+    (itemsList.baseitem || itemsList.item).forEach((item: OrigianlItem) => {
+        const parsedItem: Item = {
             id: generateRandomString(),
             name: item.name,
             description: parseDescription([parseItemDescription(item?.entries ?? [])]),
             image: null,
             limitedUsage: null,
-            action: '',
+            action: "",
             range: extractItemRange(item),
             isLegendary: false,
             isAction: false,
@@ -154,34 +182,34 @@ for await (const file of itemsDir) {
             availableCharges: item?.charges ?? 0,
             rechargeCharges: item?.recharge ?? null,
             requiresAttunement: !!item?.reqAttune,
-            attunementConditions: parseAttunementRequirements(item?.reqAttune ?? ''),
+            attunementConditions: parseAttunementRequirements(item?.reqAttune ?? ""),
             armorAC: parseItemArmor(item),
             stealthDisatvantage: !!item?.stealth,
             bonuses: parseItemBonuses(item),
             ...parseAbilityModifiers(item?.ability ?? {}),
             weaponProprieties: parseItemProperties(item?.property ?? []),
             isEquipped: false,
-        }
+        };
 
-        parsedItem.action = createItemAction(parsedItem)
+        parsedItem.action = createItemAction(parsedItem);
 
-        parsedItemList.push(parsedItem)
-    })
+        parsedItemList.push(parsedItem);
+    });
 
-    await Deno.writeTextFile(`./parsedItems/${file.name}`, JSON.stringify(parsedItemList))
+    await Deno.writeTextFile(`./parsedItems/${file.name}`, JSON.stringify(parsedItemList));
 }
 
 for await (const file of bestiaryDir) {
-    if (!file.isFile) continue
+    if (!file.isFile) continue;
 
-    const monsterList = JSON.parse(await Deno.readTextFile(`./data/bestiary/${file.name}`))
+    const monsterList = JSON.parse(await Deno.readTextFile(`./data/bestiary/${file.name}`));
 
-    if (!monsterList?.monster) continue
+    if (!monsterList?.monster) continue;
 
-    const parsedMonsterList: Monster[] = []
+    const parsedMonsterList: Monster[] = [];
 
     monsterList.monster.forEach((element: OriginalMonster) => {
-        const proficiency = getProficiencyScore(element)
+        const proficiency = getProficiencyScore(element);
         // extractTraitDebug(element)
 
         const parsedMonster: Monster = {
@@ -216,10 +244,12 @@ for await (const file of bestiaryDir) {
             legendaryActionCount: (element?.legendary?.length ?? 0) > 0 ? 3 : 0,
             legendaryResistanceCount: (element?.legendary?.length ?? 0) > 0 ? 3 : 0,
             overrides: null,
-        }
+            spellcasting: null,
+            items: [],
+        };
 
-        parsedMonsterList.push(parsedMonster)
+        parsedMonsterList.push(parsedMonster);
     });
 
-    await Deno.writeTextFile(`./parsedMonsters/${file.name}`, JSON.stringify(parsedMonsterList))
+    await Deno.writeTextFile(`./parsedMonsters/${file.name}`, JSON.stringify(parsedMonsterList));
 }
