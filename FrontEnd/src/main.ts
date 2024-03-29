2b6a9aecb1311349efb2d3ec0a8c22fadb43ecd4
ionut <hadaionut@gmail.com>
2024-03-29 08:32:31 +0200

    formating

--- FrontEnd/src/main.ts
+++ FrontEnd/src/main.ts
@@ -17,8 +17,6 @@ import {autoAnimatePlugin} from '@formkit/auto-animate/vue'
 import {QuillEditor} from '@vueup/vue-quill'
 import FloatingVue from 'floating-vue'
 import 'floating-vue/dist/style.css'
-
-
 const MainPage = () => import('./pages/MainPage.vue')
 const GamePage = () => import('./pages/GamePage.vue')
 
