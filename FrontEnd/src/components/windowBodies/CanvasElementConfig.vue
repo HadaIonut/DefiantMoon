7df5d8871e4b1c9974f2fa20c00a9948a0c40719
ionut <hadaionut@gmail.com>
2024-05-03 20:54:03 +0300

    automatic form gen from entity shape

--- FrontEnd/src/components/windowBodies/CanvasElementConfig.vue
+++ FrontEnd/src/components/windowBodies/CanvasElementConfig.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import {usePlayAreaStore} from 'src/stores/PlayArea'
 
-const nameToObjectMap = {
+type MapTypes = Record<string, 'canvasLights' | 'canvasPlayers' | 'canvasWalls'>
+
+const nameToObjectMap: MapTypes = {
   'sourceLight': 'canvasLights',
   'player': 'canvasPlayers',
   'adjustableShape': 'canvasWalls',
@@ -9,16 +11,32 @@ const nameToObjectMap = {
   'controlPoint': 'canvasWalls',
   'centerPoint': 'canvasWalls',
 }
+const filteredKeyWords = ['id', 'type']
+
+const containedAnyInArray = (testString: string, testArray: string[]): boolean => {
+  return testArray.reduce((acc, cur) => acc || testString.toLowerCase().includes(cur), false)
+}
 
 const props = defineProps<{ bodyData: {id: string, name: string} }>()
 const playAreaStore = usePlayAreaStore()
-const storeObject = playAreaStore[nameToObjectMap[props.bodyData.name]][props.bodyData.id]
-console.log(playAreaStore[nameToObjectMap[props.bodyData.name]])
-console.log(props.bodyData.id)
+const entityName = nameToObjectMap[props.bodyData.name]
+const storeObject = playAreaStore[entityName][props.bodyData.id]
+
+const variablesToConfig = Object.keys(storeObject)
+  .filter((entry) => !containedAnyInArray(entry, filteredKeyWords)) as (keyof typeof storeObject)[]
 </script>
 
 <template>
-  <div>there will be configs {{storeObject}}</div>
+  <div>
+    <div v-for="variable in variablesToConfig" :key="variable">
+      <div v-if="typeof storeObject[variable] !== 'object'">
+        <span>{{variable}}</span>
+        <input v-if="variable === 'color'" type="color" :value="`#${storeObject[variable].toString(16)}`">
+        <input v-else-if="typeof storeObject[variable] === 'boolean'" type="checkbox" :value="storeObject[variable]">
+        <input v-else :type="typeof storeObject[variable]" :value="storeObject[variable]">
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped lang="scss">
