3dd313c6c869fdffad70e64c77e4018c62e1f581
XeoSmartHome <xeosmarthome@gmail.com>
2024-06-07 15:28:43 +0300

    Only me and God does understand what we did here, and at the moment of reading this I probably already forgot

--- FrontEnd/src/components/windowBodies/CanvasElementConfig.vue
+++ FrontEnd/src/components/windowBodies/CanvasElementConfig.vue
@@ -1,7 +1,10 @@
 <script setup lang="ts">
 import {usePlayAreaStore} from 'src/stores/PlayArea'
+import {CanvasLightProperties, CanvasPlayerProperties, CanvasWallProperties} from 'src/types/PlayerArea'
 
-type MapTypes = Record<string, 'canvasLights' | 'canvasPlayers' | 'canvasWalls'>
+type EntityKey = 'sourceLight' | 'player' | 'adjustableShape' | 'wall' | 'controlPoint' | 'centerPoint'
+type EntityName = 'canvasLights' | 'canvasPlayers' | 'canvasWalls'
+type MapTypes = Record<EntityKey, EntityName>
 
 const playAreaStore = usePlayAreaStore()
 
@@ -22,19 +25,26 @@ const objectToFunctionMap = {
 
 const filteredKeyWords = ['id', 'type', 'networkupdate']
 
-const currentEntry = (variable: any) => {
-  // @ts-ignore
-  return playAreaStore[entityName][props.bodyData.id][variable]
+// TODO: move to utils
+type ValueOf<T> = T[keyof T];
+type SecondParameter<T> = T extends (param1: any, param2: infer P) => any ? P : never;
+type KeysOfUnion<T> = T extends any ? keyof T : never;
+
+type Variables = SecondParameter<ValueOf<typeof objectToFunctionMap>>;
+type VariableName = KeysOfUnion<Variables>;
+
+const currentEntry = (variable: VariableName) => {
+  return (playAreaStore[entityName][props.bodyData.id] as Record<VariableName, any>)[variable]
 }
 
 const containedAnyInArray = (testString: string, testArray: string[]): boolean => {
   return testArray.reduce((acc, cur) => acc || testString.toLowerCase().includes(cur), false)
 }
 
-const props = defineProps<{ bodyData: { id: string, name: string } }>()
+const props = defineProps<{ bodyData: { id: string, name: EntityKey } }>()
 const entityName = nameToObjectMap[props.bodyData.name]
 
-const variablesToConfig = Object.keys(playAreaStore[entityName][props.bodyData.id])
+const variablesToConfig = (Object.keys(playAreaStore[entityName][props.bodyData.id]) as VariableName[])
   .filter((entry) => {
     const contained = !containedAnyInArray(entry, filteredKeyWords)
     const isObject = typeof currentEntry(entry) === 'object'
@@ -42,7 +52,7 @@ const variablesToConfig = Object.keys(playAreaStore[entityName][props.bodyData.i
     return contained && !isObject
   })
 
-const handleInputChange = (event: Event, changedField: string) => {
+const handleInputChange = (event: Event, changedField: VariableName) => {
   const target = event.target as HTMLInputElement
   let newValue
   switch (target.type) {
@@ -50,6 +60,7 @@ const handleInputChange = (event: Event, changedField: string) => {
     newValue = Number(target.value)
     break
   case 'color':
+    // @ts-ignore
     newValue = parseInt(Number(`0${target.value.replace('#', 'x')}`), 10)
     break
   default:
@@ -64,20 +75,20 @@ const handleInputChange = (event: Event, changedField: string) => {
   })
 }
 
-const getInputType = (variable: string) => {
+const getInputType = (variable: VariableName) => {
   if (variable === 'color') return 'color'
   else if (typeof currentEntry(variable) === 'boolean') return 'checkbox'
   else return typeof currentEntry(variable)
 }
 
-const getValue = (variable: string) => {
+const getValue = (variable: VariableName) => {
   if (variable === 'color') return `#${currentEntry(variable).toString(16)}`
   else return currentEntry(variable)
 }
 </script>
 
 <template>
-  <labeledInput :labelText="variable" :inputType="getInputType(variable)" :value="getValue(variable)"
-    v-for="variable in variablesToConfig" :key="variable"
-    @input="(event: Event) => handleInputChange(event, variable)" />
+  <labeledInput :labelText="variable.toString()" :inputType="getInputType(variable)" :value="getValue(variable)"
+                v-for="variable in variablesToConfig" :key="variable"
+                @input="(event: Event) => handleInputChange(event, variable)"/>
 </template>
